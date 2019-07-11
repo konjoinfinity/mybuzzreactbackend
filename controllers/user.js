14a3ef6e6d6c9e35cc6a4a6e5a104c56235216c8
Wesley Scholl <clients@konjoweb.com>
2019-07-11 15:16:50 -0400

    changed backend routes to / and removed all req.param.ids from urls, able to use decodedId after token has been verified

--- controllers/user.js
+++ controllers/user.js
@@ -69,7 +69,104 @@ router.get("/", verifyToken, (req, res) => {
     if (err)
       return res.status(500).send("There was a problem finding the user.");
     if (!user) return res.status(404).send("No user found.");
-    User.find({}).then(users => res.json(users));
+    var currentTime = new Date();
+    var totalBac;
+    var duration;
+    User.findOne({ _id: decodedId }).then(user => {
+      if (user.buzzes.length >= 1) {
+        duration = singleDuration(user.buzzes[0].dateCreated);
+        totalBac = getBAC(
+          user.weight,
+          user.gender,
+          user.buzzes.length,
+          "Liquor",
+          duration
+        );
+        totalBac = parseFloat(totalBac.toFixed(6));
+        if (totalBac <= 0) {
+          for (i = 0; i < user.buzzes.length; i++) {
+            var oldBuzz = {
+              numberOfDrinks: 1,
+              drinkType: user.buzzes[i].drinkType,
+              hours: 1,
+              dateCreated: user.buzzes[i].dateCreated
+            };
+            var oldBuzzId = { _id: user.buzzes[i]._id };
+            User.findOneAndUpdate(
+              { _id: decodedId },
+              { $pull: { buzzes: oldBuzzId } }
+            ).then(user => {
+              user.oldbuzzes.push(oldBuzz);
+              user.save((err, user) => {
+                console.log("Moved buzz to old");
+              });
+            });
+          }
+          if (user.oldbuzzes.length >= 1) {
+            User.findOne({ _id: decodedId }).then(user => {
+              var date2 = currentTime.getTime();
+              var date1 = user.oldbuzzes[
+                user.oldbuzzes.length - 1
+              ].dateCreated.getTime();
+              var dayHourMin = getDayHourMin(date1, date2);
+              var days = dayHourMin[0];
+              var hours = dayHourMin[1];
+              var minutes = dayHourMin[2];
+              var seconds = dayHourMin[3];
+              user.timeSince = `${days} days, ${hours} hours, ${minutes} minutes, and ${seconds} seconds`;
+              user.bac = 0;
+              user.save((err, user) => {
+                res.json(user);
+              });
+            });
+          } else {
+            User.findOne({ _id: decodedId }).then(user => {
+              user.bac = 0;
+              user.timeSince = "";
+              user.save((err, user) => {
+                res.json(user);
+              });
+            });
+          }
+        } else {
+          User.findOne({ _id: decodedId }).then(user => {
+            user.bac = totalBac;
+            user.save((err, user) => {
+              res.json(user);
+            });
+          });
+        }
+      } else {
+        if (user.oldbuzzes.length >= 1) {
+          User.findOne({ _id: decodedId }).then(user => {
+            var date2 = currentTime.getTime();
+            var date1 = user.oldbuzzes[
+              user.oldbuzzes.length - 1
+            ].dateCreated.getTime();
+            var dayHourMin = getDayHourMin(date1, date2);
+            var days = dayHourMin[0];
+            var hours = dayHourMin[1];
+            var minutes = dayHourMin[2];
+            var seconds = dayHourMin[3];
+            if ((user.buzzes.length = 0)) {
+              user.bac = 0;
+            }
+            user.timeSince = `${days} days, ${hours} hours, ${minutes} minutes, and ${seconds} seconds`;
+            user.save((err, user) => {
+              res.json(user);
+            });
+          });
+        } else {
+          if (user.buzzes.length == 0) {
+            user.timeSince = "";
+            user.bac = 0;
+          }
+          user.save((err, user) => {
+            res.json(user);
+          });
+        }
+      }
+    });
   });
 });
 
@@ -148,113 +245,7 @@ router.post("/login", (req, res) => {
   }
 });
 
-router.get("/user/:id", verifyToken, (req, res) => {
-  User.findById(decodedId, { password: 0 }, (err, user) => {
-    if (err)
-      return res.status(500).send("There was a problem finding the user.");
-    if (!user) return res.status(404).send("No user found.");
-    var currentTime = new Date();
-    var totalBac;
-    var duration;
-    User.findOne({ _id: req.params.id }).then(user => {
-      if (user.buzzes.length >= 1) {
-        duration = singleDuration(user.buzzes[0].dateCreated);
-        totalBac = getBAC(
-          user.weight,
-          user.gender,
-          user.buzzes.length,
-          "Liquor",
-          duration
-        );
-        totalBac = parseFloat(totalBac.toFixed(6));
-        if (totalBac <= 0) {
-          for (i = 0; i < user.buzzes.length; i++) {
-            var oldBuzz = {
-              numberOfDrinks: 1,
-              drinkType: user.buzzes[i].drinkType,
-              hours: 1,
-              dateCreated: user.buzzes[i].dateCreated
-            };
-            var oldBuzzId = { _id: user.buzzes[i]._id };
-            User.findOneAndUpdate(
-              { _id: req.params.id },
-              { $pull: { buzzes: oldBuzzId } }
-            ).then(user => {
-              user.oldbuzzes.push(oldBuzz);
-              user.save((err, user) => {
-                console.log("Moved buzz to old");
-              });
-            });
-          }
-          if (user.oldbuzzes.length >= 1) {
-            User.findOne({ _id: req.params.id }).then(user => {
-              var date2 = currentTime.getTime();
-              var date1 = user.oldbuzzes[
-                user.oldbuzzes.length - 1
-              ].dateCreated.getTime();
-              var dayHourMin = getDayHourMin(date1, date2);
-              var days = dayHourMin[0];
-              var hours = dayHourMin[1];
-              var minutes = dayHourMin[2];
-              var seconds = dayHourMin[3];
-              user.timeSince = `${days} days, ${hours} hours, ${minutes} minutes, and ${seconds} seconds`;
-              user.bac = 0;
-              user.save((err, user) => {
-                res.json(user);
-              });
-            });
-          } else {
-            User.findOne({ _id: req.params.id }).then(user => {
-              user.bac = 0;
-              user.timeSince = "";
-              user.save((err, user) => {
-                res.json(user);
-              });
-            });
-          }
-        } else {
-          User.findOne({ _id: req.params.id }).then(user => {
-            user.bac = totalBac;
-            user.save((err, user) => {
-              res.json(user);
-            });
-          });
-        }
-      } else {
-        if (user.oldbuzzes.length >= 1) {
-          User.findOne({ _id: req.params.id }).then(user => {
-            var date2 = currentTime.getTime();
-            var date1 = user.oldbuzzes[
-              user.oldbuzzes.length - 1
-            ].dateCreated.getTime();
-            var dayHourMin = getDayHourMin(date1, date2);
-            var days = dayHourMin[0];
-            var hours = dayHourMin[1];
-            var minutes = dayHourMin[2];
-            var seconds = dayHourMin[3];
-            if ((user.buzzes.length = 0)) {
-              user.bac = 0;
-            }
-            user.timeSince = `${days} days, ${hours} hours, ${minutes} minutes, and ${seconds} seconds`;
-            user.save((err, user) => {
-              res.json(user);
-            });
-          });
-        } else {
-          if (user.buzzes.length == 0) {
-            user.timeSince = "";
-            user.bac = 0;
-          }
-          user.save((err, user) => {
-            res.json(user);
-          });
-        }
-      }
-    });
-  });
-});
-
-router.post("/user/:id", verifyToken, (req, res) => {
+router.post("/", verifyToken, (req, res) => {
   User.findById(decodedId, { password: 0 }, (err, user) => {
     if (err)
       return res.status(500).send("There was a problem finding the user.");
@@ -268,7 +259,7 @@ router.post("/user/:id", verifyToken, (req, res) => {
       hours: 0,
       dateCreated: dateTime
     };
-    User.findOne({ _id: req.params.id }).then(user => {
+    User.findOne({ _id: decodedId }).then(user => {
       user.buzzes.push(newBuzz);
       user.save().then(user => {
         if (user.buzzes.length == 0) {
@@ -294,14 +285,14 @@ router.post("/user/:id", verifyToken, (req, res) => {
   });
 });
 
-router.get("/user/:id/bac", verifyToken, (req, res) => {
+router.get("/bac", verifyToken, (req, res) => {
   User.findById(decodedId, { password: 0 }, (err, user) => {
     if (err)
       return res.status(500).send("There was a problem finding the user.");
     if (!user) return res.status(404).send("No user found.");
     var totalBac;
     var duration;
-    User.findOne({ _id: req.params.id }).then(user => {
+    User.findOne({ _id: decodedId }).then(user => {
       if (user.buzzes.length >= 1) {
         duration = singleDuration(user.buzzes[0].dateCreated);
         totalBac = getBAC(
@@ -333,14 +324,14 @@ router.get("/user/:id/bac", verifyToken, (req, res) => {
   });
 });
 
-router.put("/user/:id/del", verifyToken, (req, res) => {
+router.put("del", verifyToken, (req, res) => {
   User.findById(decodedId, { password: 0 }, (err, user) => {
     if (err)
       return res.status(500).send("There was a problem finding the user.");
     if (!user) return res.status(404).send("No user found.");
     var buzzId = { _id: req.body.index };
     User.findOneAndUpdate(
-      { _id: req.params.id },
+      { _id: decodedId },
       { $pull: { buzzes: buzzId } }
     ).then(user => {
       if (user.buzzes.length == 1) {
@@ -353,14 +344,14 @@ router.put("/user/:id/del", verifyToken, (req, res) => {
   });
 });
 
-router.put("/user/:id/olddel", verifyToken, (req, res) => {
+router.put("olddel", verifyToken, (req, res) => {
   User.findById(decodedId, { password: 0 }, (err, user) => {
     if (err)
       return res.status(500).send("There was a problem finding the user.");
     if (!user) return res.status(404).send("No user found.");
     var buzzId = { _id: req.body.index };
     User.findOneAndUpdate(
-      { _id: req.params.id },
+      { _id: decodedId },
       { $pull: { oldbuzzes: buzzId } }
     ).then(user => {
       if (user.buzzes.length == 1) {
@@ -373,30 +364,29 @@ router.put("/user/:id/olddel", verifyToken, (req, res) => {
   });
 });
 
-router.put("/user/:id/delall", verifyToken, (req, res) => {
+router.put("delall", verifyToken, (req, res) => {
   User.findById(decodedId, { password: 0 }, (err, user) => {
     if (err)
       return res.status(500).send("There was a problem finding the user.");
     if (!user) return res.status(404).send("No user found.");
-    User.findOneAndUpdate(
-      { _id: req.params.id },
-      { $pull: { buzzes: {} } }
-    ).then(user => {
-      user.bac = 0;
-      user.save((err, user) => {
-        res.json(user);
-      });
-    });
+    User.findOneAndUpdate({ _id: decodedId }, { $pull: { buzzes: {} } }).then(
+      user => {
+        user.bac = 0;
+        user.save((err, user) => {
+          res.json(user);
+        });
+      }
+    );
   });
 });
 
-router.put("/user/:id/olddelall", verifyToken, (req, res) => {
+router.put("olddelall", verifyToken, (req, res) => {
   User.findById(decodedId, { password: 0 }, (err, user) => {
     if (err)
       return res.status(500).send("There was a problem finding the user.");
     if (!user) return res.status(404).send("No user found.");
     User.findOneAndUpdate(
-      { _id: req.params.id },
+      { _id: decodedId },
       { $pull: { oldbuzzes: {} } }
     ).then(user => {
       if (user.buzzes.length == 1) {
